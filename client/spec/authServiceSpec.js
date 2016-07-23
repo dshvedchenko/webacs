f6c3490f073ca4384b9329e9851b197a535da95b
dshvedchenko <dshvedchenko@gmail.com>
2016-07-23 15:39:52 +0300

    -- fix authService login spec

--- client/spec/authServiceSpec.js
+++ client/spec/authServiceSpec.js
@@ -1,10 +1,11 @@
-describe('My first controller', function () {
-    var coreService, httpBackend, resultToken;
+describe('authService : ', function () {
+    var authService, httpBackend, resultToken, ENDPOINT_URI;
     beforeEach(module('app'));
 
-    beforeEach(inject(function (_coreService_, $httpBackend) {
-        coreService = _coreService_
+    beforeEach(inject(function (_authService_, $httpBackend, _ENDPOINT_URI_) {
+        authService = _authService_
         httpBackend = $httpBackend
+        ENDPOINT_URI = _ENDPOINT_URI_;
     }));
 
     afterEach(function () {
@@ -18,12 +19,12 @@ describe('My first controller', function () {
             expect(resultToken).toEqual('122345332')
         }
 
-        httpBackend.expect('POST', backend_server + "/api/v1/login")
+        httpBackend.expect('POST', ENDPOINT_URI + "/login")
             .respond({
                 "data": {"token": "122345332"}
             });
 
-        coreService.login({'username': 'admin', 'password': '12345'}, onSuccess)
+        authService.login({'username': 'admin', 'password': '12345'}, onSuccess)
         httpBackend.flush();
     })
 
